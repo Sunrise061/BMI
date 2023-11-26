a147e59ee991632b1d987a8255113e675ddb19d3
ST26-217\Student <ryszard.kuczowic@chorzow.merito.pl>
2023-11-26 17:48:12 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BMI_niedziela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kg, height, bmi;
+
+            Console.WriteLine("Podaj swoją wagę(W kg) ");
+            kg = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj swój wzrost(W metrach) ");
+            height = Double.Parse(Console.ReadLine());
+
+            bmi = (kg / (height * height));
+            Console.WriteLine(Math.Round(bmi, 2));
+            Console.ReadKey();
+
+        }
+    }
+}
